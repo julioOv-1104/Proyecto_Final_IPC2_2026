2f8b85d915cb44d262a09c8466fcab1781460821
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2026-04-23 15:38:28 -0600

    creacion de los proyectos de angular y netbeans para forntend y backend

---
+++ Proyecto_Final_IPC2_Backend/src/main/java/paquete/proyecto_final_ipc2_backend/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package paquete.proyecto_final_ipc2_backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ Proyecto_Final_IPC2_Backend/src/main/java/paquete/proyecto_final_ipc2_backend/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package paquete.proyecto_final_ipc2_backend.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
